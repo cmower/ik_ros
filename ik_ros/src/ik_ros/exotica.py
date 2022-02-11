615229a63d2306e714b300c5d04087080667c2c3
Chris E. Mower <chris.mower@ed.ac.uk>
2022-02-11 16:39:20 +0000

    Support EXOTica scipy solver

--- ik_ros/src/ik_ros/exotica.py
+++ ik_ros/src/ik_ros/exotica.py
@@ -1,6 +1,8 @@
 import rospy
 import pyexotica as exo
 import exotica_core_task_maps_py
+import exotica_scipy_solver
+
 """
 
 This provides a base-class for the EXOTica interface. In order to
@@ -14,6 +16,7 @@ class Exotica(IK):
 
     """Interface to Exotica: https://github.com/ipab-slmc/exotica"""
 
+
     def __init__(self):
 
         # Setup class attributes
@@ -21,9 +24,15 @@ class Exotica(IK):
 
         # Get ROS parameters
         xml_filename = rospy.get_param('~exotica_xml_filename')
+        use_scipy_solver = rospy.get_param('~use_scipy_solver', False)
+        scipy_solver_method = rospy.get_param('~scipy_solver_method', 'SLSQP')
 
         # Setup Exotica
-        self.solver = exo.Setup.load_solver(xml_filename)
+        self.problem = exo.Setup.load_problem(xml_filename)
+        if use_scipy_solver:
+            self.solver = exotica_scipy_solver.SciPyEndPoseSolver(problem=self.problem, method=scipy_solver_method, debug=False)
+        else:
+            self.solver = self.problem.get_solver()
         self.problem = self.solver.get_problem()
         self.scene = self.problem.get_scene()
         self.task_maps = self.problem.get_task_maps()
@@ -36,6 +45,7 @@ class Exotica(IK):
 
     def joint_names(self):
         return self._joint_names
+
 
     def solution(self):
         return self._solution
