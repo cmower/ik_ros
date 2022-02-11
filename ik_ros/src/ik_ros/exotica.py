59fb17a04ca3ae4d60fefa0009a99a4566ca4165
Chris E. Mower <chris.mower@ed.ac.uk>
2022-02-11 15:46:50 +0000

    Start interface for Exotica

---
+++ ik_ros/src/ik_ros/exotica.py
@@ -0,0 +1,31 @@
+import rospy
+import pyexotica as exo
+import exotica_core_task_maps_py
+
+class Exotica(IK):
+
+    """Interface to Exotica: https://github.com/ipab-slmc/exotica"""
+
+    def __init__(self):
+
+        # Setup class attributes
+        self._solution = None
+
+        # Get ROS parameters
+        xml_filename = rospy.get_param('~exotica_xml_filename')
+
+        # Setup Exotica
+        self.solver = exo.Setup.load_solver(xml_filename)
+        self.problem = self.solver.get_problem()
+        self.scene = self.problem.get_scene()
+        self.task_maps = self.problem.get_task_maps()
+        self._joint_names = self.scene.get_controlled_joint_names()
+
+    def solve(self):
+        self._solution = self.solver.solve()[0]
+
+    def joint_names(self):
+        return self._joint_names
+
+    def solution(self):
+        return self._solution
