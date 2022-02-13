0706adf534ba1f70607ee355ca6cb2784243b834
Chris Mower <chris.mower@ed.ac.uk>
2022-02-13 01:10:55 +0000

    Implement more appropriate node for fig8 example

--- ik_ros_examples/src/ik_ros_examples/utils.py
+++ ik_ros_examples/src/ik_ros_examples/utils.py
@@ -1,4 +1,35 @@
-import math
+import rospy
+import tf2_ros
+import numpy as np
+from geometry_msgs.msg import TransformStamped
 
-def lwr_figure_eight(t):
-    return [0.6, -0.1 + math.sin(t * 2.0 * math.pi * 0.5) * 0.1, 0.5 + math.sin(t * math.pi * 0.5) * 0.2, 0.0, 0.0, 0.0, 1.0]
+class FigureEightNode:
+
+    def __init__(self):
+        rospy.init_node('figure_eight_node')
+        parent_frame_id = rospy.get_param('~parent_frame_id')
+        child_frame_id = rospy.get_param('~child_frame_id')
+        hz = rospy.get_param('~hz', 50)
+        self.tfBroadcaster = tf2_ros.TransformBroadcaster()
+        self.tf = TransformStamped()
+        self.tf.header.frame_id = parent_frame_id
+        self.tf.child_frame_id = child_frame_id
+        self.tf.transform.rotation.w = 1.0
+        rospy.Timer(rospy.Duration(1.0/float(hz)), self.main_loop)
+
+    def main_loop(self, event):
+
+        # Grab current time
+        time_now = rospy.Time.now()
+        self.tf.header.stamp = time_now
+
+        # Update transform
+        t = time_now.to_sec()
+        self.tf.transform.translation.x = np.sin(t * 2.0 * np.pi * 0.5) * 0.1
+        self.tf.transform.translation.y = np.sin(t * np.pi * 0.5) * 0.2
+
+        # Broadcast transform
+        self.tfBroadcaster.sendTransform(self.tf)
+
+    def spin(self):
+        rospy.spin()
