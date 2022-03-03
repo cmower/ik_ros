a1251e858e0ec82d74921bc3905213b5b02dd229
Chris E. Mower <chris.mower@ed.ac.uk>
2022-03-03 17:15:28 +0000

    Add toggle service

--- ik_ros/scripts/tf_to_floatarray_node.py
+++ ik_ros/scripts/tf_to_floatarray_node.py
@@ -1,34 +1,84 @@
 #!/usr/bin/env python3
 import rospy
 import tf2_ros
+from std_srvs.srv import SetBool, SetBoolResponse
 from std_msgs.msg import Float64MultiArray
 
+# import rospy
+# from std_srvs.srv import SetBool, SetBoolResponse
+from rospy.impl.tcpros_base import DEFAULT_BUFF_SIZE
 
-def main():
+class ToggleService(rospy.Service):
 
-    # Setup
-    rospy.init_node('tf_to_floatarray_node', anonymous=True)
-    pub = rospy.Publisher('transform', Float64MultiArray, queue_size=10)
-    tfBuffer = tf2_ros.Buffer()
-    listener = tf2_ros.TransformListener(tfBuffer)
-    child_frame_id = rospy.get_param('~child_frame_id')
-    parent_frame_id = rospy.get_param('~parent_frame_id')
-    hz = rospy.get_param('~hz', 50)
-    rate = rospy.Rate(hz)
+    def __init__(self, name, enable_handler, disable_handler,
+                 buff_size=DEFAULT_BUFF_SIZE, error_handler=None):
+        super(ToggleService, self).__init__(name, SetBool, self.toggle,
+                                            buff_size=buff_size, error_handler=error_handler)
+        self.enable_handler = enable_handler
+        self.disable_handler = disable_handler
 
-    # Main loop
-    while not rospy.is_shutdown():
+    def toggle(self, req):
+        if req.data:
+            success, message = self.enable_handler()
+        else:
+            success, message = self.disable_handler()
+        return SetBoolResponse(success=success, message=message)
+
+class Node:
+
+    def __init__(self):
+        rospy.init_node('tf_to_floatarray_node', anonymous=True)
+        self.pub = rospy.Publisher('transform', Float64MultiArray, queue_size=10)
+        self.tfBuffer = tf2_ros.Buffer()
+        self.listener = tf2_ros.TransformListener(self.tfBuffer)
+        self.child_frame_id = rospy.get_param('~child_frame_id')
+        self.parent_frame_id = rospy.get_param('~parent_frame_id')
+        hz = rospy.get_param('~hz', 50)
+        self.dt = 1.0/float(hz)
+        # rospy.Service('toggle_tf_to_floatarray', SetBool, self.toggle)
+        self.sub = None
+        self.timer = None
+        ToggleService('toggle_tf_to_floatarray', self.start, self.stop)
+
+    # def toggle(self, req):
+    #     if req.data:
+    #         success, message = self.start()
+    #     else:
+    #         success, message = self.stop()
+    #     return SetBoolResponse(message=message, success=success)
+
+    def start(self):
+        if (self.sub is None) and (self.timer is None):
+            self.timer = rospy.Timer(rospy.Duration(self.dt), self.main_loop)
+            success = True
+            message = 'started tf_to_floatarray node'
+        else:
+            success = False
+            message = 'user attempted to start tf_to_floatarray node, but it is already running!'
+            rospy.logerr(message)
+        return success, message
+
+    def stop(self):
+        if (self.sub is not None) and (self.timer is not None):
+            self.timer.shutdown()
+            success = True
+            message = 'stopped tf_to_floatarray node'
+        else:
+            success = False
+            message = 'user attempted to stop tf_to_floatarray node, but it is not running!'
+        return success, message
+
+    def main_loop(self, event):
 
         # Grab transform
         try:
-            tf = tfBuffer.lookup_transform(parent_frame_id, child_frame_id, rospy.Time())
+            tf = self.tfBuffer.lookup_transform(self.parent_frame_id, self.child_frame_id, rospy.Time())
         except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
-            rospy.logwarn('did not recieve transform %s in %s', child_frame_id, parent_frame_id)
-            rate.sleep()
-            continue
+            rospy.logwarn('did not recieve transform %s in %s', self.child_frame_id, self.parent_frame_id)
+            return
 
         # Publish transform as float array
-        pub.publish(Float64MultiArray(data=[
+        self.pub.publish(Float64MultiArray(data=[
             tf.transform.translation.x,
             tf.transform.translation.y,
             tf.transform.translation.z,
@@ -38,8 +88,12 @@ def main():
             tf.transform.rotation.w,
         ]))
 
-        # Sleep
-        rate.sleep()
+    def spin(self):
+        rospy.spin()
+
+
+def main():
+    Node().spin()
 
 
 if __name__ == '__main__':
