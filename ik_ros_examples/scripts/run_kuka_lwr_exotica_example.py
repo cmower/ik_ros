f493c2600d42ca4eb7ee5567075a787216b9c10b
Chris E. Mower <chris.mower@ed.ac.uk>
2022-03-08 14:00:10 +0000

    Add joint smoothing task map

--- ik_ros_examples/scripts/run_kuka_lwr_exotica_example.py
+++ ik_ros_examples/scripts/run_kuka_lwr_exotica_example.py
@@ -10,15 +10,22 @@ class Node:
     def __init__(self):
         rospy.init_node('run_lwr_exotica_example')
         self.pub = rospy.Publisher('ik', EXOTicaProblem, queue_size=10)
+        self.previous_solutions = []
         self.start_state = rospy.wait_for_message('rpbi/kuka_lwr/joint_states', JointState)
-        rospy.Subscriber('rpbi/kuka_lwr/joint_states', JointState, self.callback)
+        rospy.Subscriber('rpbi/kuka_lwr/joint_states', JointState, self.joint_state_callback)
+        rospy.Subscriber('joint_states/target', JointState, self.target_joint_state_callback)
         rospy.Timer(rospy.Duration(1.0/float(rospy.get_param('~hz', 100))), self.loop)
 
-    def callback(self, msg):
+    def joint_state_callback(self, msg):
         self.start_state = msg
 
+    def target_joint_state_callback(self, msg):
+        self.previous_solutions.append(msg)
+        if len(self.previous_solutions) == 2:
+            self.previous_solutions.pop(0)
+
     def loop(self, event):
-        self.pub.publish(EXOTicaProblem(start_state=self.start_state))
+        self.pub.publish(EXOTicaProblem(start_state=self.start_state, previous_solutions=self.previous_solutions))
 
     def spin(self):
         rospy.spin()
