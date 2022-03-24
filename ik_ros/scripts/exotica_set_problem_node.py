a313e6758aeb81403e25f79d19f77281138d031c
Chris Mower <chris.mower@ed.ac.uk>
2022-03-24 21:28:59 +0000

    Clean up implementation

--- ik_ros/scripts/exotica_set_problem_node.py
+++ ik_ros/scripts/exotica_set_problem_node.py
@@ -11,25 +11,38 @@ from custom_srvs.custom_srvs import ToggleService
 class Node:
 
     def __init__(self):
+
+        # Init node
         rospy.init_node('exotica_set_problem_node')
         self.exotica_problem = EXOTicaProblem()
+
+        # Get parameters
+        self.dt = 1.0/float(rospy.get_param('~hz', 50))
         self.max_num_prev_solutions = rospy.get_param('~max_num_prev_solutions', 1)
+
+        # Get information on exotica
         rospy.wait_for_service('exotica_info')
         get_exotica_info = rospy.ServiceProxy('exotica_info', EXOTicaInfo)
         self.exotica_info = get_exotica_info()
-        self.dt = 1.0/float(rospy.get_param('~hz', 50))
+
+        # Setup class variables
         self.timer = None
         self.start_state = None
         self.previous_solutions = []
-
         self.task_map_goals = {}
+
+        # Setup subscribers
         for idx, task_map_name in enumerate(self.exotica_info.task_map_names):
             self.task_map_goals[task_map_name] = None
             rospy.Subscriber('exotica_set_problem/%s'%task_map_name, Float64MultiArray, self.task_map_callback, callback_args=task_map_name)
 
         rospy.Subscriber('previous_solutions', JointState, self.previous_solutions_callback)
         rospy.Subscriber('start_state', JointState, self.start_state_callback)
+
+        # Final initialization
         ToggleService('toggle_exotica_set_problem_node', self.start, self.stop)
+        if rospy.get_param('~start_on_init', False):
+            self.start()
 
     def task_map_callback(self, msg, task_map_name):
         self.task_map_goals[task_map_name] = msg.data
@@ -43,13 +56,25 @@ class Node:
         self.start_state = msg
 
     def start(self):
-        self.timer = rospy.Timer(rospy.Duration(self.dt), self.loop)
-        return True, 'started exotica_set_problem_node'
+        if self.timer is None:
+            self.timer = rospy.Timer(rospy.Duration(self.dt), self.loop)
+            success = True
+            message = 'started exotica_set_problem_node'
+        else:
+            success = False
+            message = 'tried to start exotica_set_problem_node but is is already running'
+        return success, message
 
     def stop(self):
-        self.timer.shutdown()
-        self.timer = None
-        return True, 'stopped exotica_set_problem_node'
+        if self.timer is not None:
+            self.timer.shutdown()
+            self.timer = None
+            success = True
+            message = 'stopped exotica_set_problem_node'
+        else:
+            success = False
+            message = 'tried to stop exotica_set_problem_node but is is not running'
+        return success, message
 
     def loop(self, event):
         exotica_problem = EXOTicaProblem()
@@ -61,7 +86,6 @@ class Node:
             if goal is not None:
                 exotica_problem.task_map_names.append(task_map_name)
                 exotica_problem.task_map_goals.append(goal)
-
 
         exotica_problem.header.stamp = rospy.Time.now()
         self.pub.publish(exotica_problem)
