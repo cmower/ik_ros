97e20fb6d75f0f2fe7945a0c17d164417c7cb76b
Chris Mower <chris.mower@ed.ac.uk>
2022-03-25 10:24:00 +0000

    Use ToggleService from custom_srvs pkg

--- ik_ros_examples/src/ik_ros_examples/utils.py
+++ ik_ros_examples/src/ik_ros_examples/utils.py
@@ -2,24 +2,7 @@ import rospy
 import tf2_ros
 import numpy as np
 from geometry_msgs.msg import TransformStamped
-from std_srvs.srv import SetBool, SetBoolResponse
-from rospy.impl.tcpros_base import DEFAULT_BUFF_SIZE
-
-class ToggleService(rospy.Service):
-
-    def __init__(self, name, enable_handler, disable_handler,
-                 buff_size=DEFAULT_BUFF_SIZE, error_handler=None):
-        super(ToggleService, self).__init__(name, SetBool, self.toggle,
-                                            buff_size=buff_size, error_handler=error_handler)
-        self.enable_handler = enable_handler
-        self.disable_handler = disable_handler
-
-    def toggle(self, req):
-        if req.data:
-            success, message = self.enable_handler()
-        else:
-            success, message = self.disable_handler()
-        return SetBoolResponse(success=success, message=message)
+from custom_srvs.custom_srvs import ToggleService
 
 class FigureEightNode:
 
