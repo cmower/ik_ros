9b3d8004a06dd2d6daa0a7b5604c8c18ae69b10a
Chris Mower <chris.mower@ed.ac.uk>
2022-03-26 23:51:00 +0000

    Add short description

--- ik_ros/src/ik_ros/ik_solver_interfaces.py
+++ ik_ros/src/ik_ros/ik_solver_interfaces.py
@@ -4,6 +4,14 @@ from .exotica_setup import EXOTicaSetupNode
 from .trac_ik import TracIKInterface
 from .exotica import EXOTicaInterface
 
+"""
+
+This script defines a dictionary containing the IK interfaces. The
+solver_interfaces dictionary is used by scripts/ik_setup_node.py and
+scripts/ik_solver_node.py to load the desired interface.
+
+"""
+
 __all__ = ['solver_interfaces']
 
 class SolverInterface:
