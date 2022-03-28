94f756d12f62e688fe85a4c4b48fe7aa43715b91
Chris Mower <chris.mower@ed.ac.uk>
2022-03-28 02:35:54 +0100

    Add type hints

--- ik_ros/src/ik_ros/ik_interface.py
+++ ik_ros/src/ik_ros/ik_interface.py
@@ -1,4 +1,7 @@
 from abc import ABCMeta, abstractmethod
+from typing import List
+from ik_output import IKOutput
+from sensor_msgs.msg import JointState
 
 """
 This script defines the main template class for an IK interface.
@@ -11,39 +14,43 @@ class IK(metaclass=ABCMeta):
 
     @property
     @abstractmethod
-    def name(self):
+    def name(self) -> str:
         pass
 
     @property
     @abstractmethod
     def problem_msg_type(self):
+        # Output: IK problem message type (defined in ik_ros/msg)
         pass
 
     @property
     @abstractmethod
     def srv_type(self):
+        # Output: IK service type (defined in ik_ros/srv)
         pass
 
     @property
     @abstractmethod
     def srv_resp_type(self):
+        # Output: IK service response type (defined in ik_ros/srv)
         pass
 
     ###########################
     ## Abstract methods, must be specified in sub-class
 
     @abstractmethod
-    def get_joint_names(self):
+    def get_joint_names(self) -> List[str]:
         pass
 
     @abstractmethod
-    def solve(self, problem):
+    def solve(self, problem) -> IKOutput:
+        # Input: IK problem (defined in ik_ros/msg)
         pass
 
     ###########################
     ## Other methods
 
-    def resolve_joint_position_order(self, joint_state_msg):
+    def resolve_joint_position_order(self, joint_state_msg: JointState) -> List[float]:
         position = []
         for name in self.get_joint_names():
             if name in joint_state_msg.name:
