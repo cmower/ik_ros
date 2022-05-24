c2e07bf82d8fd409ee0a513e6bc1cfdd9618a3ba
stoutheo <stoutheo@gmail.com>
2022-05-24 15:15:33 -0400

    Making the figure 8 more modest, due to worspace/singularity limits

--- ik_ros_examples/src/ik_ros_examples/utils.py
+++ ik_ros_examples/src/ik_ros_examples/utils.py
@@ -4,6 +4,7 @@ from custom_ros_tools.tf import TfInterface
 from geometry_msgs.msg import TransformStamped
 from custom_ros_tools.ros_comm import ToggleService
 
+
 class FigureEightNode:
 
     def __init__(self):
@@ -63,9 +64,9 @@ class FigureEightNode:
 
         # Update transform
         # TODO: expose figure eight parameters to user
-        t = (self.tf.header.stamp - self.start_time).to_sec()*0.4
-        self.tf.transform.translation.x = np.sin(t * 2.0 * np.pi * 0.5) * 0.1
-        self.tf.transform.translation.y = np.sin(t * np.pi * 0.5) * 0.2
+        t = (self.tf.header.stamp - self.start_time).to_sec()*0.3
+        self.tf.transform.translation.x = np.sin(t * 2.0 * np.pi * 0.5) * 0.05
+        self.tf.transform.translation.y = np.sin(t * np.pi * 0.5) * 0.1
 
         # Broadcast transform
         self.tf_interface.tf_broadcaster.sendTransform(self.tf)
